4dfe938e4da9d834074eea29e8311b38bc913121
Victor Batista <victor.guedes.08@gmail.com>
2024-12-04 14:56:42 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const { readFileSync } = require('fs');
 
 function gerarFaturaStr(fatura, pecas) {
+
+    // função query
+    function getPeca(apresentacao) {
+        return pecas[apresentacao.id];
+    }
+
     // função extraída
-    function calcularTotalApresentacao(apre, peca) {
+    function calcularTotalApresentacao(apre) {
         let total = 0;
+        const peca = getPeca(apre);
         switch (peca.tipo) {
             case "tragedia":
                 total = 40000;
@@ -31,16 +38,15 @@ function gerarFaturaStr(fatura, pecas) {
         { style: "currency", currency: "BRL", minimumFractionDigits: 2 }).format;
 
     for (let apre of fatura.apresentacoes) {
-        const peca = pecas[apre.id];
-        let total = calcularTotalApresentacao(apre, peca);
+        let total = calcularTotalApresentacao(apre);
 
         // créditos para próximas contratações
         creditos += Math.max(apre.audiencia - 30, 0);
-        if (peca.tipo === "comedia")
+        if (getPeca(apre).tipo === "comedia")
             creditos += Math.floor(apre.audiencia / 5);
 
         // mais uma linha da fatura
-        faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+        faturaStr += `  ${getPeca(apre).nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
         totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
